b61340f84c80470e2ae91af2370e1b3868a4cfb7
Jonathan Heathcote <mail@jhnet.co.uk>
2011-01-04 00:20:53 +0000

    Added tropo SMS client.

---
+++ sms.py
@@ -0,0 +1,20 @@
+from base_module import *
+import re
+import urllib, urllib2
+
+requiredBeardBotVersion = 0.1
+class BeardBotModule(ModuleBase):
+	"""help [module_name]
+	"""
+	@on_addressed_match("(?:sms|txt|text) (\w+) (.*)", re.I)
+	def text(self, source_name, source_host, message, name, msg):
+		params = {
+			"action": "create",
+			"token" : open("tropotoken_sms","r").read(),
+			"frm"   : source_name,
+			"name"  : name,
+			"msg"   : msg,
+		}
+		
+		url = "https://api.tropo.com/1.0/sessions?%s"%urllib.urlencode(params)
+		urllib2.urlopen(url).read()
