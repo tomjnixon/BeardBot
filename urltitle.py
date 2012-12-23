1df80ff4cab55b72f1c6116ac52d16f30165fd27
James Sandford <jamesmsandford@googlemail.com>
2012-12-23 19:07:56 +0000

    Fixed error when no URL in message

--- urltitle.py
+++ urltitle.py
@@ -6,6 +6,7 @@ requiredBeardBotVersion = 0.1
 class BeardBotModule(ModuleBase):
 	def on_channel_message(self, source_name, source_host, message):
 		match_urls = re.compile("""((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.‌​][a-z]{2,4}/)(?:[^\s()<>]+|(([^\s()<>]+|(([^\s()<>]+)))*))+(?:(([^\s()<>]+|(‌​([^\s()<>]+)))*)|[^\s`!()[]{};:'".,<>?«»“”‘’]))""")
-		urlFound = match_urls.search(message).group(0)
-		title = lxml.html.parse(urlFound).find(".//title").text
-		self.bot.say(source_name + " linked to: " + title)
+		urlFound = match_urls.search(message)
+		if urlFound != None:
+			title = lxml.html.parse(urlFound.group(0)).find(".//title").text
+			self.bot.say(source_name + " linked to: " + title)
