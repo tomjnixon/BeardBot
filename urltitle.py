de5766708cf24b9ef5a5fce07cbba82cec195e58
James Sandford <jamesmsandford@googlemail.com>
2013-01-02 02:42:25 +0000

    Support for https

--- urltitle.py
+++ urltitle.py
@@ -1,5 +1,6 @@
 # coding=UTF-8
 from base_module import *
+from urllib2 import urlopen
 import re, lxml.html
 
 requiredBeardBotVersion = 0.1
@@ -8,5 +9,5 @@ class BeardBotModule(ModuleBase):
 		match_urls = re.compile("""((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.‌​][a-z]{2,4}/)(?:[^\s()<>]+|(([^\s()<>]+|(([^\s()<>]+)))*))+(?:(([^\s()<>]+|(‌​([^\s()<>]+)))*)|[^\s`!()[]{};:'".,<>?«»“”‘’]))""")
 		urlFound = match_urls.search(message)
 		if urlFound != None:
-			title = lxml.html.parse(urlFound.group(0)).find(".//title").text
+			title = lxml.html.parse(urlopen(urlFound.group(0))).find(".//title").text
 			self.bot.say(source_name + " linked to: " + title)
