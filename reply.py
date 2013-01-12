5ad0bdd2b8155d520e3767406ed57f1a2062df88
James Sandford <jamesmsandford@googlemail.com>
2013-01-12 23:42:00 +0000

    Reply now has functionality for actions and addressed messaged

--- reply.py
+++ reply.py
@@ -2,21 +2,45 @@ from base_module import *
 from os.path import exists
 import re, string
 
-replyFile = "./data/replyFile"
+channelReplyFile = "./data/channelReplyFile"
+actionReplyFile = "./data/actionReplyFile"
+addressReplyFile = "./data/addressReplyFile"
 
 requiredBeardBotVersion = 0.1
 class BeardBotModule(ModuleBase):
 	def __init__(self, newBot):
 		ModuleBase.__init__(self, newBot)
 		
-		self.replyList = []
+		self.channelReplyList = []
+		self.actionReplyList = []
+		self.addressReplyList = []
 		
-		if exists(replyFile):
-				for line in filter(lambda l: len(l) and l[0] != '$', map(string.strip, open(replyFile).xreadlines())):
+		if exists(channelReplyFile):
+				for line in filter(lambda l: len(l) and l[0] != '$', map(string.strip, open(channelReplyFile).xreadlines())):
 					regex, reply = map(string.strip, line.split('\t', 1))
-					self.replyList.append((re.compile(regex, re.I), reply))
-		
+					self.channelReplyList.append((re.compile(regex, re.I), reply))
+
+		if exists(actionReplyFile):
+				for line in filter(lambda l: len(l) and l[0] != '$', map(string.strip, open(actionReplyFile).xreadlines())):
+					regex, reply = map(string.strip, line.split('\t', 1))
+					self.actionReplyList.append((re.compile(regex, re.I), reply))
+
+		if exists(addressReplyFile):
+				for line in filter(lambda l: len(l) and l[0] != '$', map(string.strip, open(addressReplyFile).xreadlines())):
+					regex, reply = map(string.strip, line.split('\t', 1))
+					self.addressReplyList.append((re.compile(regex, re.I), reply))
+
 	def on_channel_message(self, source_name, source_host, message):
-		for regex, reply in self.replyList:
+		for regex, reply in self.actionReplyList:
+			if regex.search(message):
+				self.bot.say(reply)
+
+	def on_action(self, source_name, source_host, message):
+		for regex, reply in self.actionReplyList:
+			if regex.search(message):
+				self.bot.say(reply)
+
+	def on_addressed_message(self, source_name, source_host, message):
+		for regex, reply in self.addressReplyList:
 			if regex.search(message):
 				self.bot.say(reply)
